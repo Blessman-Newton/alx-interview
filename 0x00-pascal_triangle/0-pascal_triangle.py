27a07791d369d2b0628eaa85c37b82f6049827c7
Blessman Newton <102887817+Blessman-Newton@users.noreply.github.com>
2023-12-01 00:56:23 +0000

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,6 +5,9 @@
 """
 
 def pascal_triangle(n):
+     """Create a function def pascal_triangle(n): that returns a list of lists
+    of integers representing the Pascal’s triangle of n
+    """
     if n <= 0:
         return []
         
